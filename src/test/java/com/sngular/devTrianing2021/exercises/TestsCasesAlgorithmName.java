b682b7d40e2055485c1cc18db999f2a8b157404b
Tomas Uriel Hernandez Basurto <uriel.hernandez@medianetsoftware.com>
2021-06-16 16:37:15 -0500

    stripped test cases

--- src/test/java/com/sngular/devTrianing2021/exercises/TestsCasesAlgorithmName.java
+++ src/test/java/com/sngular/devTrianing2021/exercises/TestsCasesAlgorithmName.java
@@ -5,10 +5,19 @@ import static org.junit.Assert.*;
 
 public class TestsCasesAlgorithmName {
     @Test
-    public void test() {
+    public void testOne() {
         assertEquals("Test value 125485923",985543221, AlgorithmName.orderNumber(125485923));
+    }
+    @Test
+    public void testTwo() {
         assertEquals("Test value 346789925",998765432, AlgorithmName.orderNumber(346789925));
+    }
+    @Test
+    public void testThree() {
         assertEquals("Test value 29384756",98765432, AlgorithmName.orderNumber(29384756));
+    }
+    @Test
+    public void testFour() {
         assertEquals("Test value 514978236",987654321, AlgorithmName.orderNumber(514978236));
     }
 }
