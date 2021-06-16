ac2a6c86a737c98fe060a743980818a863468f9b
Tomas Uriel Hernandez Basurto <uriel.hernandez@medianetsoftware.com>
2021-06-16 13:33:23 -0500

    added some modifications on README and have been added messages

--- src/test/java/com/sngular/devTrianing2021/exercises/TestsCasesAlgorithmName.java
+++ src/test/java/com/sngular/devTrianing2021/exercises/TestsCasesAlgorithmName.java
@@ -6,9 +6,9 @@ import static org.junit.Assert.*;
 public class TestsCasesAlgorithmName {
     @Test
     public void test() {
-        assertEquals(985543221, AlgorithmName.orderNumber(125485923));
-        assertEquals(998765432, AlgorithmName.orderNumber(346789925));
-        assertEquals(98765432, AlgorithmName.orderNumber(29384756));
-        assertEquals(987654321, AlgorithmName.orderNumber(514978236));
+        assertEquals("Test value 125485923",985543221, AlgorithmName.orderNumber(125485923));
+        assertEquals("Test value 346789925",998765432, AlgorithmName.orderNumber(346789925));
+        assertEquals("Test value 29384756",98765432, AlgorithmName.orderNumber(29384756));
+        assertEquals("Test value 514978236",987654321, AlgorithmName.orderNumber(514978236));
     }
 }
