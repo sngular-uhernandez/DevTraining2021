7fff1ceaad257804c3f2725ef60e5785c0206649
Tomas Uriel Hernandez Basurto <uriel.hernandez@medianetsoftware.com>
2021-06-16 14:26:43 -0500

    stripped test cases

--- src/test/java/com/sngular/devTrianing2021/exercises/TestsCasesAlgorithmName.java
+++ src/test/java/com/sngular/devTrianing2021/exercises/TestsCasesAlgorithmName.java
@@ -1,18 +1,39 @@
-package com.sngular.devTrining2021.test.exercises;
+package com.sngular.devTrining2021.exercises;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestsCasesAlgorithmName {
     @Test
-    public void test() {
+    public void testOne() {
         assertEquals("Test value Dermatoglyphics",true, AlgorithmName.isIsogram("Dermatoglyphics"));
+    }
+    @Test
+    public void testTwo() {
         assertEquals("Test value isogram",true, AlgorithmName.isIsogram("isogram"));
+    }
+    @Test
+    public void testThree() {
         assertEquals("Test value moose",false, AlgorithmName.isIsogram("moose"));
+    }
+    @Test
+    public void testFour() {
         assertEquals("Test value isIsogram",false, AlgorithmName.isIsogram("isIsogram"));
+    }
+    @Test
+    public void testFive() {
         assertEquals("Test value aba",false, AlgorithmName.isIsogram("aba"));
+    }
+    @Test
+    public void testSix() {
         assertEquals("Test value moOse",false, AlgorithmName.isIsogram("moOse"));
+    }
+    @Test
+    public void testSeven() {;
         assertEquals("Test value thumbscrewjapingly",true, AlgorithmName.isIsogram("thumbscrewjapingly"));
-        assertEquals("Test value ''",true, AlgorithmName.isIsogram(""));
+    }
+    @Test
+    public void testEigt() {
+        assertEquals("Test value ",true, AlgorithmName.isIsogram(""));
     }
 }
