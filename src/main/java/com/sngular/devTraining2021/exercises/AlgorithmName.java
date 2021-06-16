cacb89231454c65ee65404f7a517fbfcc230704c
Tomas Uriel Hernandez Basurto <uriel.hernandez@medianetsoftware.com>
2021-06-16 13:52:04 -0500

    added some modifications on README and have been added messages

--- src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
+++ src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
@@ -2,6 +2,7 @@ package com.sngular.devTrining2021.exercises;
 
 public class AlgorithmName {
     public static String accum(String str) {
-        // your code
+        //Implements the function with the algorithm
+        return null;
     }
 }
