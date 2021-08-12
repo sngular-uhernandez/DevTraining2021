d7ee3112b091c2b5b185364290bdb6b1b1b07f13
Tomas Uriel Hernandez Basurto <uriel.hernandez@medianetsoftware.com>
2021-08-12 16:26:16 -0500

    errase algorithm

--- src/main/java/com/sngular/devTraining2021/exercises/Algorithm.java
+++ src/main/java/com/sngular/devTraining2021/exercises/Algorithm.java
@@ -2,19 +2,6 @@ package com.sngular.devTrining2021.exercises;
 
 public class Algorithm {
     public static String startOz(String str) {
-        String rstr = "";
-
-        if(str != null){
-            str = str.toLowerCase();
-
-            int i = (str.indexOf("o") > -1)? str.indexOf("o"):((str.indexOf("z") > -1)?str.indexOf("z"):-1);
-
-            if(i > -1 && str.length() > i+1 && str.charAt(i+1) == 'z'){
-                rstr = str.substring(i,i+2);
-            } else if(i > -1)
-                rstr = str.substring(i,i+1);
-        }
-
-        return rstr;
+     // implement algorith
     }
 }
